d951eb826d1a39ef9d223a182750791cd9b1521a
Mikhail <etozhenerk@yandex.ru>
2021-10-03 23:36:58 +0300

    fix

--- script/main.js
+++ script/main.js
@@ -2,11 +2,15 @@
 
 const btBnurger = document.querySelector(".btn-burger");
 const catalog = document.querySelector(".catalog");
-const overlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".btn-close");
 const subCatalog = document.querySelector(".subcatalog");
 const subCatalogHeader = document.querySelector(".subcatalog-header");
 const btnReturn = document.querySelector(".btn-return");
+
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () => {
     catalog.classList.add("open");
