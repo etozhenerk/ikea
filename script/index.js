0e5e45511143e485fa9928702a8a4cc58dcccfea
Mikhail <etozhenerk@yandex.ru>
2021-10-15 22:30:45 +0300

    add generateGoodsPage

--- script/index.js
+++ script/index.js
@@ -3,6 +3,7 @@
 import { generateHeader } from "./generateHeader.js";
 import { generateFooter } from "./generateFooter.js";
 import { generateCatalog } from "./generateCatalog.js";
+import { generateGoodsPage } from "./generateGoodsPage.js";
 import { loadData } from "./loadData.js"
 
 generateHeader();
